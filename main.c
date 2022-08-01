89a11b6b5bc050da5813eabec138486ae6620608
Oren FREOUA <orfreoua@e1r8p15.clusters.42paris.fr>
2022-08-01 17:57:44 +0200

    :...

--- main.c
+++ main.c
@@ -17,35 +17,36 @@ int	load_files(t_data *data, int argc, char **argv)
 	return (TRUE);
 }
 
-char	**load_cmds(int argc, char **argv)
+char *get_path(char **envp)
 {
-	char	**cmds;
-	int		i;
-	
-	cmds = malloc(sizeof(char *) * (argc - 2));
-	if (!cmds)
-		ft_putstr_fd("Malloc failed\n", 2);
-	else
+	while (*envp)
 	{
-		i = 2;
-		while (i < (argc - 1))
-		{
-			cmds[i - 2] = ft_strdup(argv[i]);
-			i++;
-		}
-		cmds[i - 1] = 0;
+		if (starts_with(*envp, "PATH="))
+			return *envp + 5;
 	}
-	return (cmds);
+	return (NULL);
 }
 
-//< infile cmd1 cmd2 > outfile
+char **split_path(char *path)
+{
+
+}
+
+char *find_command(char **paths, char *command)
+{
+	while (*paths)
+	{
+		char *join = join(*paths, "/", command);
+		if (access(join) == OK)
+			return 	join;
+	}
+	return (NULL);
+}
 
 int	main(int argc, char **argv, char **env)
 {
 	t_data	data;
 
-	(void)argc;
-	(void)argv;
 	(void)env;
 
 	if ((!BONUS && argc != 5) || argc < 5)
@@ -53,9 +54,9 @@ int	main(int argc, char **argv, char **env)
 		ft_putstr_fd("error wrong number of arguments\n", 2);
 		return (0);
 	}
-	data.cmds = load_cmds(argc, argv);
-	if (!data.cmds)
+	if (load_files(&data, argc, argv) != TRUE)
 		return (1);
+	
 	/*
 	if (execve(argv[2], data.cmds, env) < 0)
 		write(2, "error\n", 6);
